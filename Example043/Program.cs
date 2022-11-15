71f119b5b44f994f0da1667c415cd51a87936e9b
Egiazarov Vazgen <egiazarov.vazgen@icloud.com>
2022-11-15 16:19:31 +0300

    Example043

---
+++ Example043/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая находит точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2 
+// (значения k1, b1, k2 и b2 задаются пользователем)
+
+void IntersectionPoint()
+{
+    Console.WriteLine("Введите k1: ");
+    var k1 = Convert.ToDouble(Console.ReadLine()!);
+    Console.WriteLine("Введите b1: ");
+    var b1 = Convert.ToDouble(Console.ReadLine()!);
+    Console.WriteLine("Введите k2: ");
+    var k2 = Convert.ToDouble(Console.ReadLine()!);
+    Console.WriteLine("Введите b2: ");
+    var b2 = Convert.ToDouble(Console.ReadLine()!);
+    var x = -(b1 - b2) / (k1 - k2);
+    var y = k1 * x + b1;
+    x = Math.Round(x, 3);
+    y = Math.Round(y, 3);
+    Console.WriteLine($"Координаты точки пересечения: ({x};{y})");
+}
+
+IntersectionPoint();

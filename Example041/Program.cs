b3960dbdcf2fdc2392a60c5a37f947a73f4820e2
Egiazarov Vazgen <egiazarov.vazgen@icloud.com>
2022-11-15 15:59:43 +0300

    Example041

---
+++ Example041/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая даёт пользователю ввести с клавиатуры M чисел 
+// и считает, сколько чисел больше 0 ввёл пользователь
+
+int NumbersGreaterThanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("Количество чисел больше 0: " + count);
+    return count;
+}
+
+Console.Write("Введите числа (через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+NumbersGreaterThanZero(array);
